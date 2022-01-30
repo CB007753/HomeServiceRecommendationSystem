8ac283bf223988a9de47f418135a282cb59305be
CB007753 <cb007753@students.apiit.lk>
2022-01-31 02:03:00 +0530

    database hired user table modification

--- website/models.py
+++ website/models.py
@@ -17,16 +17,11 @@ class User(db.Model, UserMixin):
 class HiredUser(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(150))
-    nic = db.Column(db.String(150))
-    address = db.Column(db.String(500))
-    city_of_work = db.Column(db.String(150))
     telephone = db.Column(db.String(150))
-    occupation = db.Column(db.String(150))
     work = db.Column(db.String(150))
-    experience = db.Column(db.Integer)
-    age_group = db.Column(db.String(150))
-    nvq_level = db.Column(db.String(150))
+    status = db.Column(db.String(150))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    plumber_id = db.Column(db.Integer, db.ForeignKey('plumber.id'))
 
 
 class Note(db.Model):
