0cdcda2485f424f38a0e6e3f2fc24cc54a4cb84a
CB007753 <cb007753@students.apiit.lk>
2022-02-04 17:49:26 +0530

    added edit profile function and made few modification in design, test result : success

--- website/views.py
+++ website/views.py
@@ -2,8 +2,6 @@ from flask import Blueprint, render_template, request, jsonify, flash, redirect,
 from flask_login import login_required, current_user
 import json
 
-from nltk.corpus import stopwords
-
 views = Blueprint('views', __name__)
 
 
@@ -49,6 +47,48 @@ def plumbers():
 @login_required
 def view_profile():
     return render_template("profile_page.html", user=current_user)
+
+
+@views.route('/edit-profile-page', methods=['GET', 'POST'])
+@login_required
+def edit_profile_page():
+    try:
+        if request.method == 'POST':
+            email = request.form.get('email')
+            fullname = request.form.get('name')
+            city = request.form.get('city')
+            address = request.form.get('address')
+            user_id = current_user.id
+            current_user_email = current_user.email
+
+            from .models import User
+            user = User.query.filter_by(email=email).first()
+            if user and current_user_email != email:
+                flash('Email is already taken, try another email.', category='error')
+            elif len(fullname) < 3:
+                flash('Full name must be greater than 2 characters.', category='error')
+            elif len(email) < 4:
+                flash('Email must be greater than 3 characters.', category='error')
+            elif len(address) < 10:
+                flash('Address is too short.', category='error')
+            else:
+                from .models import User
+                edit_user = User.query.get(user_id)
+
+                from . import db
+
+                edit_user.fullname = fullname
+                edit_user.email = email
+                edit_user.address = address
+                edit_user.city = city
+
+                db.session.commit()
+                flash('Profile Updated Successfully.', category='success')
+                return redirect(url_for('views.view_profile'))
+    except Exception as e:
+        print(e)
+        flash('Something went wrong.', category='error')
+    return render_template("edit_profile_page.html", user=current_user)
 
 
 @views.route('/view-plumber-details/<int:record_id>', methods=['GET', 'POST'])
