d41bb45d2ad94f85b8d4654fad4835a56d89fe2e
CB007753 <cb007753@students.apiit.lk>
2022-02-06 20:13:23 +0530

    modified all functions to have 'try and except' in it

--- website/views.py
+++ website/views.py
@@ -19,28 +19,36 @@ def notification():
 
 @views.route('/delete-note', methods=['POST'])
 def delete_note():
-    from .models import Note
-    note = json.loads(request.data)
-    noteId = note['noteId']
-    note = Note.query.get(noteId)
-    if note:
-        if note.user_id == current_user.id:
-            from . import db
-            db.session.delete(note)
-            db.session.commit()
-            flash('Notification Deleted', category='success')
-        else:
-            flash('Notification couldn\'t be deleted', category='error')
+    try:
+        from .models import Note
+        note = json.loads(request.data)
+        noteId = note['noteId']
+        note = Note.query.get(noteId)
+        if note:
+            if note.user_id == current_user.id:
+                from . import db
+                db.session.delete(note)
+                db.session.commit()
+                flash('Notification Deleted', category='success')
+            else:
+                flash('Notification couldn\'t be deleted', category='error')
 
-    return jsonify({})
+        return jsonify({})
+    except Exception as e:
+        flash("Something went wrong !", category="error")
+        print(e)
 
 
 @views.route('/plumbers', methods=['GET', 'POST'])
 @login_required
 def plumbers():
-    from .models import Plumbers
-    plumbers = Plumbers.query.all()
-    return render_template("plumbers_page.html", user=current_user, plumbers=plumbers)
+    try:
+        from .models import Plumbers
+        plumbers = Plumbers.query.all()
+        return render_template("plumbers_page.html", user=current_user, plumbers=plumbers)
+    except Exception as e:
+        flash("Something went wrong !", category="error")
+        print(e)
 
 
 @views.route('/view-profile', methods=['GET', 'POST'])
@@ -94,35 +102,39 @@ def edit_profile_page():
 @views.route('/view-plumber-details/<int:record_id>', methods=['GET', 'POST'])
 @login_required
 def view_plumber_details(record_id):
-    from .models import Plumbers, WorkEngine, HiredUser
-    selected_plumber = Plumbers.query.get(record_id)
-    name = selected_plumber.name
-    plumber_work = selected_plumber.work
-    loaded_engine = WorkEngine.recommendations
-    h_user = HiredUser.query.filter_by(name=name).first()
-    if h_user:
-        status = 'On A Hire'
-    else:
-        status = 'Available'
+    try:
+        from .models import Plumbers, WorkEngine, HiredUser
+        selected_plumber = Plumbers.query.get(record_id)
+        name = selected_plumber.name
+        plumber_work = selected_plumber.work
+        loaded_engine = WorkEngine.recommendations
+        h_user = HiredUser.query.filter_by(name=name).first()
+        if h_user:
+            status = 'On A Hire'
+        else:
+            status = 'Available'
 
-    id_1 = loaded_engine(plumber_work)[0][0]
-    id_2 = loaded_engine(plumber_work)[0][1]
-    id_3 = loaded_engine(plumber_work)[0][2]
-    id_4 = loaded_engine(plumber_work)[0][3]
-    id_5 = loaded_engine(plumber_work)[0][4]
-    id_6 = loaded_engine(plumber_work)[0][5]
+        id_1 = loaded_engine(plumber_work)[0][0]
+        id_2 = loaded_engine(plumber_work)[0][1]
+        id_3 = loaded_engine(plumber_work)[0][2]
+        id_4 = loaded_engine(plumber_work)[0][3]
+        id_5 = loaded_engine(plumber_work)[0][4]
+        id_6 = loaded_engine(plumber_work)[0][5]
 
-    recommended_plumber_1 = Plumbers.query.get(id_1)
-    recommended_plumber_2 = Plumbers.query.get(id_2)
-    recommended_plumber_3 = Plumbers.query.get(id_3)
-    recommended_plumber_4 = Plumbers.query.get(id_4)
-    recommended_plumber_5 = Plumbers.query.get(id_5)
-    recommended_plumber_6 = Plumbers.query.get(id_6)
+        recommended_plumber_1 = Plumbers.query.get(id_1)
+        recommended_plumber_2 = Plumbers.query.get(id_2)
+        recommended_plumber_3 = Plumbers.query.get(id_3)
+        recommended_plumber_4 = Plumbers.query.get(id_4)
+        recommended_plumber_5 = Plumbers.query.get(id_5)
+        recommended_plumber_6 = Plumbers.query.get(id_6)
 
-    return render_template("selected_plumber.html", user=current_user, plumber=selected_plumber,
-                           rec_plumber_1=recommended_plumber_1, rec_plumber_2=recommended_plumber_2,
-                           rec_plumber_3=recommended_plumber_3, rec_plumber_4=recommended_plumber_4,
-                           rec_plumber_5=recommended_plumber_5, rec_plumber_6=recommended_plumber_6, status=status)
+        return render_template("selected_plumber.html", user=current_user, plumber=selected_plumber,
+                               rec_plumber_1=recommended_plumber_1, rec_plumber_2=recommended_plumber_2,
+                               rec_plumber_3=recommended_plumber_3, rec_plumber_4=recommended_plumber_4,
+                               rec_plumber_5=recommended_plumber_5, rec_plumber_6=recommended_plumber_6, status=status)
+    except Exception as e:
+        flash("Something went wrong !", category="error")
+        print(e)
 
 
 @views.route('/hire-me/<int:record_id>', methods=['GET', 'POST'])
@@ -172,28 +184,38 @@ def hire_me(record_id):
             return redirect(url_for("views.current_hiring"))
     except Exception as e:
         print(e)
+        flash("Something went wrong !", category="error")
         return redirect(url_for("views.current_hiring"))
 
 
 @views.route('/current-hiring', methods=['GET', 'POST'])
 @login_required
 def current_hiring():
-    from .models import HiredUser
-    current_hired_user = HiredUser.query.all()
-    return render_template("current_hiring.html", user=current_user, hired=current_hired_user)
+    try:
+        from .models import HiredUser
+        current_hired_user = HiredUser.query.all()
+        return render_template("current_hiring.html", user=current_user, hired=current_hired_user)
+
+    except Exception as e:
+        flash("Something went wrong !", category="error")
+        print(e)
 
 
 @views.route('/details-of-hired-plumber/<int:record_id>', methods=['GET', 'POST'])
 @login_required
 def view_hired_plumber_details(record_id):
-    from .models import Plumbers, HiredUser
+    try:
+        from .models import Plumbers, HiredUser
 
-    status_of_hired_plumber = HiredUser.query.get(record_id)
-    plumber_id = status_of_hired_plumber.plumber_id
+        status_of_hired_plumber = HiredUser.query.get(record_id)
+        plumber_id = status_of_hired_plumber.plumber_id
 
-    details_of_hired_plumber = Plumbers.query.get(plumber_id)
-    return render_template("view_details_hired_plumber.html", user=current_user, plumber=details_of_hired_plumber,
-                           hired_plumber=status_of_hired_plumber)
+        details_of_hired_plumber = Plumbers.query.get(plumber_id)
+        return render_template("view_details_hired_plumber.html", user=current_user, plumber=details_of_hired_plumber,
+                               hired_plumber=status_of_hired_plumber)
+    except Exception as e:
+        flash("Something went wrong !", category="error")
+        print(e)
 
 
 @views.route('/update-arrived/<int:record_id>', methods=['GET', 'POST'])
@@ -286,9 +308,14 @@ def work_completed(record_id):
 @views.route('/hired-history', methods=['GET', 'POST'])
 @login_required
 def hired_history():
-    from .models import HiredHistory
-    hired_plumber_history = HiredHistory.query.all()
-    return render_template("completed_hiring.html", user=current_user, hired=hired_plumber_history)
+    try:
+        from .models import HiredHistory
+        hired_plumber_history = HiredHistory.query.all()
+        return render_template("completed_hiring.html", user=current_user, hired=hired_plumber_history)
+
+    except Exception as e:
+        flash("Something went wrong !", category="error")
+        print(e)
 
 
 @views.route('/details-of-hired-plumber-history/<int:record_id>', methods=['GET', 'POST'])
@@ -336,38 +363,42 @@ def view_hired_plumber_history_details(record_id):
 @views.route('/recommend-based-on-city', methods=['GET', 'POST'])
 @login_required
 def recommend_based_on_city():
-    from .models import CityEngine
-    loaded_engine = CityEngine.recommendations
-    from .models import Plumbers
-    city = current_user.city
-    id_1 = loaded_engine(city)[0][0]
-    id_2 = loaded_engine(city)[0][1]
-    id_3 = loaded_engine(city)[0][2]
-    id_4 = loaded_engine(city)[0][3]
-    id_5 = loaded_engine(city)[0][4]
-    id_6 = loaded_engine(city)[0][5]
-    id_7 = loaded_engine(city)[0][6]
-    id_8 = loaded_engine(city)[0][7]
-    id_9 = loaded_engine(city)[0][8]
-    id_10 = loaded_engine(city)[0][9]
+    try:
+        from .models import CityEngine
+        loaded_engine = CityEngine.recommendations
+        from .models import Plumbers
+        city = current_user.city
+        id_1 = loaded_engine(city)[0][0]
+        id_2 = loaded_engine(city)[0][1]
+        id_3 = loaded_engine(city)[0][2]
+        id_4 = loaded_engine(city)[0][3]
+        id_5 = loaded_engine(city)[0][4]
+        id_6 = loaded_engine(city)[0][5]
+        id_7 = loaded_engine(city)[0][6]
+        id_8 = loaded_engine(city)[0][7]
+        id_9 = loaded_engine(city)[0][8]
+        id_10 = loaded_engine(city)[0][9]
 
-    recommended_plumber_1 = Plumbers.query.get(id_1)
-    recommended_plumber_2 = Plumbers.query.get(id_2)
-    recommended_plumber_3 = Plumbers.query.get(id_3)
-    recommended_plumber_4 = Plumbers.query.get(id_4)
-    recommended_plumber_5 = Plumbers.query.get(id_5)
-    recommended_plumber_6 = Plumbers.query.get(id_6)
-    recommended_plumber_7 = Plumbers.query.get(id_7)
-    recommended_plumber_8 = Plumbers.query.get(id_8)
-    recommended_plumber_9 = Plumbers.query.get(id_9)
-    recommended_plumber_10 = Plumbers.query.get(id_10)
+        recommended_plumber_1 = Plumbers.query.get(id_1)
+        recommended_plumber_2 = Plumbers.query.get(id_2)
+        recommended_plumber_3 = Plumbers.query.get(id_3)
+        recommended_plumber_4 = Plumbers.query.get(id_4)
+        recommended_plumber_5 = Plumbers.query.get(id_5)
+        recommended_plumber_6 = Plumbers.query.get(id_6)
+        recommended_plumber_7 = Plumbers.query.get(id_7)
+        recommended_plumber_8 = Plumbers.query.get(id_8)
+        recommended_plumber_9 = Plumbers.query.get(id_9)
+        recommended_plumber_10 = Plumbers.query.get(id_10)
 
-    return render_template("city_plumbers_recommendation.html", user=current_user,
-                           plumber_1=recommended_plumber_1, plumber_2=recommended_plumber_2,
-                           plumber_3=recommended_plumber_3, plumber_4=recommended_plumber_4,
-                           plumber_5=recommended_plumber_5, plumber_6=recommended_plumber_6,
-                           plumber_7=recommended_plumber_7, plumber_8=recommended_plumber_8,
-                           plumber_9=recommended_plumber_9, plumber_10=recommended_plumber_10)
+        return render_template("city_plumbers_recommendation.html", user=current_user,
+                               plumber_1=recommended_plumber_1, plumber_2=recommended_plumber_2,
+                               plumber_3=recommended_plumber_3, plumber_4=recommended_plumber_4,
+                               plumber_5=recommended_plumber_5, plumber_6=recommended_plumber_6,
+                               plumber_7=recommended_plumber_7, plumber_8=recommended_plumber_8,
+                               plumber_9=recommended_plumber_9, plumber_10=recommended_plumber_10)
+    except Exception as e:
+        print(e)
+        flash("Something went wrong", category="error")
 
 
 @views.route('/recommend-based-on-interest', methods=['GET', 'POST'])
